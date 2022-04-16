2895d591337c35a92ac32437ea0cca04a965d52e
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-04-16 22:13:58 +0100

    Passar dados para a View, Atualização do ficheiro config e Implementação do Bootstrap, CSS e JS

--- website/app/controllers/HomeController.php
+++ website/app/controllers/HomeController.php
@@ -5,6 +5,12 @@ class HomeController extends Controller
     public function index()
     {
         //echo 'home/index';
-        $this->view('pages/home');
+
+        $dados = [
+          'teste1' => 'Teste 1',
+          'teste2' => 'Teste 2'
+        ];
+
+        $this->view('pages/home', $dados);
     }
 }
---
+++ website/app/views/pages/home.php
@@ -0,0 +1,2 @@
+<?php echo $dados['teste1'] ?>
+<?php echo $dados['teste2'] ?>
