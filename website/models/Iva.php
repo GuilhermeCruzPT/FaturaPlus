54e66e8e979d7ab45a081707f877cfe2238d32cf
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-05-23 21:22:20 +0100

    Backoffice Users - Model

--- website/models/Iva.php
+++ website/models/Iva.php
@@ -2,5 +2,11 @@
 
 class Iva extends \ActiveRecord\Model
 {
-
+    /* verifica a precença
+         não pode ser nulo*/
+    static $validates_presence_of = array(
+        array('percentage', 'message' => 'A percentage não pode estar vazio'),
+        array('description', 'message' => 'A description não pode estar vazio'),
+        array('vigour', 'message' => 'A vigour não pode estar vazio'),
+    );
 }
