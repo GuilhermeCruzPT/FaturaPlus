3c0f6d22020d94de9e7fad15c59aae34a33009e7
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-05-24 20:43:42 +0100

    Backoffice Enterprises - Atualização do Create

--- website/views/backoffice/enterprises/create.php
+++ website/views/backoffice/enterprises/create.php
@@ -1,133 +1,139 @@
-
 <!DOCTYPE html>
-<html >
+<html>
 <head>
-    <title >Create</title>
+    <title >Criar Empresa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?= DIRPAGE ?>public/css/backoffice.css" rel="stylesheet">
+    <link href="<?= DIRCSS ?>backoffice.css" rel="stylesheet">
 </head>
 <body>
-<div class="container">
-    <div class="box" style=" margin: 200px;">
+<section class="home-section">
+    <div class="container">
+        <div class="box" style=" margin: 200px;">
 
-    <form action="router.php?c=bill&a=save" method="post" style="
+            <form action="router.php?c=enterprises&a=save" method="post" style="
     width: 1000px;
 	padding: 20px;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
 
-        <h4 class="display-4 text-center">Create</h4><hr><br>
+                <h4 class="display-4 text-center">Criar Empresa</h4><hr><br>
 
-        <?php
-       /* if (isset($products)){
-        if ($products->errors->on('referencia')) {
-            echo "<font color='red'>" . $products->errors->on('referencia') . "</font>";
-        }}*/
-        ?>
+                <div class="form-group">
+                    <label for="social_designation">Designação Social:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="social_designation"
+                           name="social_designation"
+                           placeholder="Inserir Designação Social">
+                </div>
 
-        <div class="form-group">
-            <label for="Designação_social">designação_social</label>
-            <input type="text"
-                   class="form-control"
-                   id="social_designation"
-                   name="designação_social"
-                   placeholder="Enter designação_social">
+                <br>
+
+                <div class="form-group">
+                    <label for="email">E-mail:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="email"
+                           name="email"
+                           placeholder="Inserir E-mail">
+                </div>
+
+                <br>
+
+                <div class="form-group">
+                    <label for="phone">Número de Telemóvel:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="phone"
+                           name="phone"
+                           placeholder="Inserir Número de Telemóvel">
+                </div>
+
+                <br>
+
+                <div class="form-group">
+                    <label for="nif">Nif:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="nif"
+                           name="nif"
+                           placeholder="Inserir Nif">
+                </div>
+
+                <br>
+
+                <div class="form-group">
+                    <label for="postal_code">Código Postal:</label>
+                    <input type="number"
+                           class="form-control"
+                           id="postal_code"
+                           name="postal_code"
+                           placeholder="Inserir Código Postal">
+                </div>
+
+                <br>
+
+                <div class="form-group">
+                    <label for="country">País:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="country"
+                           name="country"
+                           placeholder="Inserir País">
+                </div>
+
+                <br>
+
+                <div class="form-group">
+                    <label for="city">Cidade:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="city"
+                           name="city"
+                           placeholder="Inserir Cidade">
+                </div>
+
+                <br>
+
+                <div class="form-group">
+                    <label for="locale">Localidade:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="locale"
+                           name="locale"
+                           placeholder="Inserir Localidade">
+                </div>
+
+                <br>
+
+                <div class="form-group">
+                    <label for="address">Morada:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="address"
+                           name="address"
+                           placeholder="Inserir Morada">
+                </div>
+
+                <br>
+
+                <div class="form-group">
+                    <label for="social_capital">Capital Social:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="social_capital"
+                           name="social_capital"
+                           placeholder="Inserir Capital Social">
+                </div>
+
+                <br>
+
+                <button type="submit"
+                        class="btn btn-primary"
+                        name="create">Criar</button>
+
+            </form>
         </div>
-
-
-        <div class="form-group">
-            <label for="Email">email</label>
-            <input type="text"
-                   class="form-control"
-                   id="email"
-                   name="email"
-                   placeholder="Enter email">
-        </div>
-
-        <div class="form-group">
-            <label for="Phone">phone</label>
-            <input type="text"
-                   class="form-control"
-                   id="phone"
-                   name="phone"
-                   placeholder="Enter phone">
-        </div>
-
-        <div class="form-group">
-            <label for="NIF">nif</label>
-            <input type="text"
-                   class="form-control"
-                   id="nif"
-                   name="nif"
-                   placeholder="Enter nif">
-        </div>
-
-
-
-        <div class="form-group">
-            <label for="Postal_code">codigo_postal</label>
-            <input type="number"
-                   class="form-control"
-                   id="postal_code"
-                   name="codigo_postal"
-                   placeholder="Enter Código postal">
-        </div>
-
-
-        <div class="form-group">
-            <label for="country">País</label>
-            <input type="text"
-                   class="form-control"
-                   id="country"
-                   name="country"
-                   placeholder="Enter country">
-        </div>
-
-        <div class="form-group">
-            <label for="city">Cidade</label>
-            <input type="text"
-                   class="form-control"
-                   id="city"
-                   name="city"
-                   placeholder="Enter city">
-        </div>
-
-
-
-        <div class="form-group">
-            <label for="locale">Localidade</label>
-            <input type="text"
-                   class="form-control"
-                   id="locale"
-                   name="locale"
-                   placeholder="Enter locale">
-        </div>
-
-
-        <div class="form-group">
-            <label for="address">Morada</label>
-            <input type="text"
-                   class="form-control"
-                   id="address"
-                   name="address"
-                   placeholder="Enter address">
-        </div>
-
-
-        <div class="form-group">
-            <label for="social_capital">Capital social</label>
-            <input type="text"
-                   class="form-control"
-                   id="social_capital"
-                   name="social_capital"
-                   placeholder="Enter social_capital">
-        </div>
-
-
-        <button type="submit"
-                class="btn btn-primary"
-                name="create">Create</button>
-    </form>
-</div>
-</div>
+    </div>
+</section>
 </body>
 </html>
