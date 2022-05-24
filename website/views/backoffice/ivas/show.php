4f3636edf3e63cfda470dcb6a594e9201bbf6913
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-05-24 09:33:46 +0100

    Backoffice Iva - Show

--- website/views/backoffice/ivas/show.php
+++ website/views/backoffice/ivas/show.php
@@ -1,28 +1,22 @@
 <!DOCTYPE html>
-<html >
+<html>
 <head>
-    <title >Criar Iva</title>
+    <title>Mostrar Iva</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?= DIRCSS ?>backoffice.css" rel="stylesheet">
 </head>
-<body>
+
 <section class="home-section">
+    <body>
     <div class="container">
         <div class="box" style=" margin: 200px; background: white;">
 
-            <form action="router.php?c=ivas&a=save" method="post" style="
+            <form action="router.php?c=ivas&a=index" method="post" style="
     width: 1000px;
 	padding: 20px;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
 
-                <h4 class="display-4 text-center">Criar Iva</h4><hr><br>
-
-                <?php
-                /* if (isset($products)){
-                 if ($products->errors->on('referencia')) {
-                     echo "<font color='red'>" . $products->errors->on('referencia') . "</font>";
-                 }}*/
-                ?>
+                <h4 class="display-4 text-center">Mostrar Iva</h4><hr><br>
 
                 <div class="form-group">
                     <label for="percentage">Percentagem:</label>
@@ -30,7 +24,7 @@
                            class="form-control"
                            id="percentage"
                            name="percentage"
-                           placeholder="Inserir Percentagem">
+                           value="<?= $iva->percentage ?>">
                 </div>
 
                 <br>
@@ -41,27 +35,29 @@
                            class="form-control"
                            id="description"
                            name="description"
-                           placeholder="Inserir Descrição">
+                           value="<?= $iva->description ?>">
                 </div>
 
                 <br>
 
                 <div class="form-group">
                     <label for="vigour">Vigor:</label>
-                    <select class="form-control" id="vigour" name="vigour">
-                        <option value="1">Com Vigor</option>
-                        <option value="0">Sem Vigor</option>
-                    </select>
+                    <input type="text"
+                           class="form-control"
+                           id="vigour"
+                           name="vigour"
+                           value="<?= $iva->vigour ?>">
                 </div>
 
                 <br>
 
                 <button type="submit"
                         class="btn btn-primary"
-                        name="create">Criar</button>
+                        name="return">Voltar</button>
+
             </form>
         </div>
     </div>
+    </body>
 </section>
-</body>
 </html>
