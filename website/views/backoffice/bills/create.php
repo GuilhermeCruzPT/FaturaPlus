106a780381bd5ebbf7384fd9ee0fc5020180dbcb
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-05-24 10:33:06 +0100

    Backoffice Bill - Atualização do Index

--- website/views/backoffice/bills/create.php
+++ website/views/backoffice/bills/create.php
@@ -6,94 +6,94 @@
     <link href="<?= DIRCSS ?>backoffice.css" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
-        <div class="box" style=" margin: 200px;">
+<div class="container">
+    <div class="box" style=" margin: 200px;">
 
-            <form action="router.php?c=bills&a=save" method="post" style="
+        <form action="router.php?c=bills&a=save" method="post" style="
             width: 1000px;
             padding: 20px;
             box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
 
-                <h4 class="display-4 text-center">Criar Fatura</h4><hr><br>
+            <h4 class="display-4 text-center">Criar Fatura</h4><hr><br>
 
-                <?php
-               /* if (isset($products)){
-                if ($products->errors->on('referencia')) {
-                    echo "<font color='red'>" . $products->errors->on('referencia') . "</font>";
-                }}*/
-                ?>
+            <?php
+            /* if (isset($products)){
+             if ($products->errors->on('referencia')) {
+                 echo "<font color='red'>" . $products->errors->on('referencia') . "</font>";
+             }}*/
+            ?>
 
-                <div class="form-group">
-                    <label for="date">Data:</label>
-                    <input type="date"
-                           class="form-control"
-                           id="date"
-                           name="date"
-                           placeholder="Inserir Data">
-                </div>
+            <div class="form-group">
+                <label for="date">Data:</label>
+                <input type="date"
+                       class="form-control"
+                       id="date"
+                       name="date"
+                       placeholder="Inserir Data">
+            </div>
 
-                <br>
+            <br>
 
-                <div class="form-group">
-                    <label for="total_value">Valor Total:</label>
-                    <input type="number"
-                           class="form-control"
-                           id="total_value"
-                           name="total_value"
-                           placeholder="Inserir Valor Total">
-                </div>
+            <div class="form-group">
+                <label for="total_value">Valor Total:</label>
+                <input type="number"
+                       class="form-control"
+                       id="total_value"
+                       name="total_value"
+                       placeholder="Inserir Valor Total">
+            </div>
 
-                <br>
+            <br>
 
-                <div class="form-group">
-                    <label for="total_iva">Iva Total:</label>
-                    <input type="text"
-                           class="form-control"
-                           id="total_iva"
-                           name="total_iva"
-                           placeholder="Inserir Iva Total">
-                </div>
+            <div class="form-group">
+                <label for="total_iva">Iva Total:</label>
+                <input type="text"
+                       class="form-control"
+                       id="total_iva"
+                       name="total_iva"
+                       placeholder="Inserir Iva Total">
+            </div>
 
-                <br>
+            <br>
 
-                <div class="form-group">
-                    <label for="state">Estado:</label>
-                    <input type="text"
-                           class="form-control"
-                           id="state"
-                           name="state"
-                           placeholder="Inserir Estado">
-                </div>
+            <div class="form-group">
+                <label for="state">Estado:</label>
+                <input type="text"
+                       class="form-control"
+                       id="state"
+                       name="state"
+                       placeholder="Inserir Estado">
+            </div>
 
-                <br>
+            <br>
 
-                <div class="form-group">
-                    <label for="client_reference_id">Referência Cliente:</label>
-                    <input type="text"
-                           class="form-control"
-                           id="client_reference_id"
-                           name="client_reference_id"
-                           placeholder="Inserir Referência Cliente">
-                </div>
+            <div class="form-group">
+                <label for="client_reference_id">Referência Cliente:</label>
+                <input type="text"
+                       class="form-control"
+                       id="client_reference_id"
+                       name="client_reference_id"
+                       placeholder="Inserir Referência Cliente">
+            </div>
 
-                <br>
+            <br>
 
-                <div class="form-group">
-                    <label for="employee_reference_id">Referência Funcionário:</label>
-                    <input type="text"
-                           class="form-control"
-                           id="employee_reference_id"
-                           name="employee_reference_id"
-                           placeholder="Inserir Referência Funcionário">
-                </div>
+            <div class="form-group">
+                <label for="employee_reference_id">Referência Funcionário:</label>
+                <input type="text"
+                       class="form-control"
+                       id="employee_reference_id"
+                       name="employee_reference_id"
+                       placeholder="Inserir Referência Funcionário">
+            </div>
 
-                <br>
+            <br>
 
-                <button type="submit"
-                        class="btn btn-primary"
-                        name="create">Criar</button>
-            </form>
-        </div>
+            <button type="submit"
+                    class="btn btn-primary"
+                    name="create">Criar</button>
+        </form>
     </div>
+</div>
 </body>
 </html>
--- website/views/backoffice/bills/index.php
+++ website/views/backoffice/bills/index.php
@@ -1,6 +1,5 @@
 <html>
 <head>
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?= DIRCSS ?>backoffice.css" rel="stylesheet">
@@ -9,59 +8,57 @@
 
 <section class="home-section">
 
-        <h4 class="display-4 align-text-top" style="text-indent: 50px; padding-top: 25px;">Bill</h4><br>
-        <div class="container">
-            <div class="box">
+    <h4 class="display-4 align-text-top" style="text-indent: 50px; padding-top: 25px;">Faturas</h4><br>
+    <div class="container">
+        <div class="box">
 
-        <form method="post" action="router.php?c=bill&a=index">
-            <input type="text" placeholder="Search.." name="search">
-            <button name="search_btn" type="submit"><i class="fa fa-search"></i></button>
-        </form>
-        <br>
-        <table class="table table-striped" style="background: white">
-            <thead>
-            <tr>
-                <th scope="col">id</th>
-                <th scope="col">date</th>
-                <th scope="col">total_value</th>
-                <th scope="col">total_iva</th>
-                <th scope="col">state</th>
-                <th scope="col">client_id</th>
-                <th scope="col">employee_id</th>
-                <th scope="col">acções disponiveis</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($bill
+            <form method="post" action="router.php?c=bills&a=index">
+                <input type="text" placeholder="Search.." name="search">
+                <button name="search_btn" type="submit"><i class="fa fa-search"></i></button>
+            </form>
+            <br>
+            <table class="table table-striped" style="background: white">
+                <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Data</th>
+                    <th scope="col">Valor Total</th>
+                    <th scope="col">Iva Total</th>
+                    <th scope="col">Estado</th>
+                    <th scope="col">Referência Cliente</th>
+                    <th scope="col">Referência Funcionário</th>
+                    <th scope="col">Ações Disponiveis</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($bills as $bill) { ?>
 
-            as $bill) { ?>
+                <td><?= $bill->id ?></td>
+                <td><?= $bill->date->format('Y-m-d'); ?></td>
+                <td><?= $bill->total_value ?></td>
+                <td><?= $bill->total_iva ?></td>
+                <td><?= $bill->state ?></td>
+                <td><?= $bill->client_reference_id ?></td>
+                <td><?= $bill->employee_reference_id ?></td>
 
-            <td><?= $bill->id ?></td>
-            <td><?= $bill->date->format('Y-m-d'); ?></td>
-            <td><?= $bill->total_value ?></td>
-            <td><?= $bill->total_iva ?></td>
-            <td><?= $bill->state ?></td>
-            <td><?= $bill->client_reference_id ?></td>
-            <td><?= $bill->employee_reference_id ?></td>
+                <td>
+                    <a href="router.php?c=bills&a=show&id=<?= $bill->id ?>"
+                       class="btn btn-primary">Mostrar</a>
 
-            <td>
-                <a href="router.php?c=bill&a=show&id=<?= $bill->id ?>"
-                   class="btn btn-primary">Show</a>
+                    <a href="router.php?c=bills&a=edit&id=<?= $bill->id ?>"
+                       class="btn btn-success">Atualizar</a>
 
-                <a href="router.php?c=bill&a=edit&id=<?= $bill->id ?>"
-                   class="btn btn-success">Update</a>
-
-                <a href="router.php?c=bill&a=delete&id=<?= $bill->id ?>"
-                   class="btn btn-danger">Delete</a>
-            </td>
-            </tr>
-            </tbody>
-            <?php } ?> </table>
-        <div class="btn btn-success">
-            <a href="router.php?c=bill&a=create" class="btn btn-success">Create</a>
+                    <a href="router.php?c=bills&a=delete&id=<?= $bill->id ?>"
+                       class="btn btn-danger">Eliminar</a>
+                </td>
+                </tr>
+                </tbody>
+                <?php } ?> </table>
+            <div class="btn btn-success">
+                <a href="router.php?c=bills&a=create" class="btn btn-success">Criar</a>
+            </div>
         </div>
     </div>
-</div>
 </section>
 </body>
 </html>
