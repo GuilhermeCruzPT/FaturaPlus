85646f3a893b4c83e0a8b9b0bf693967fb63ba65
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-05-24 10:44:10 +0100

    Backoffice Bill - Atualização do Show

--- website/views/backoffice/bills/show.php
+++ website/views/backoffice/bills/show.php
@@ -1,87 +1,97 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Show</title>
+    <title>Mostrar Fatura</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?= DIRPAGE ?>public/css/backoffice.css" rel="stylesheet">
+    <link href="<?= DIRCSS ?>backoffice.css" rel="stylesheet">
 </head>
 <body>
-<div class="container">
-    <div class="box" style=" margin: 200px;">
 
-        <form action="router.php?c=bill&a=index" method="post" style="
+<section class="home-section">
+
+    <div class="container">
+        <div class="box" style=" margin: 200px;">
+
+            <form action="router.php?c=bills&a=index" method="post" style="
     width: 1000px;
 	padding: 20px;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
 
-            <h4 class="display-4 text-center">Show</h4>
-            <hr>
-            <br>
+                <h4 class="display-4 text-center">Mostrar Fatura</h4>
 
-            <div class="form-group">
-                <label for="date">date</label>
-                <input type="text"
-                       class="form-control"
-                       id="date"
-                       name="date"
-                       value="<?= $bill->data ?>">
-            </div>
+                <div class="form-group">
+                    <label for="date">Data:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="date"
+                           name="date"
+                           value="<?= $bill->data ?>">
+                </div>
 
-            <div class="form-group">
-                <label for="total_value">total_value</label>
-                <input type="text"
-                       class="form-control"
-                       id="total_value"
-                       name="total_value"
-                       value="<?= $bill->valor_total ?>">
-            </div>
+                <br>
 
-            <div class="form-group">
-                <label for="total_iva">total_iva</label>
-                <input type="text"
-                       class="form-control"
-                       id="total_iva"
-                       name="total_iva"
-                       value="<?= $bill->iva_total ?>">
-            </div>
+                <div class="form-group">
+                    <label for="total_value">Valor Total:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="total_value"
+                           name="total_value"
+                           value="<?= $bill->valor_total ?>">
+                </div>
 
-            <div class="form-group">
-                <label for="state">state</label>
-                <input type="text"
-                       class="form-control"
-                       id="state"
-                       name="state"
-                       value="<?= $bill->estado ?>">
-            </div>
+                <br>
 
-            <div class="form-group">
-                <label for="client_reference_id">client_reference_id</label>
-                <input type="text"
-                       class="form-control"
-                       id="client_reference_id"
-                       name="client_reference_id"
-                       value="<?= $bill->referencia_cliente ?>">
-            </div>
+                <div class="form-group">
+                    <label for="total_iva">Iva Total:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="total_iva"
+                           name="total_iva"
+                           value="<?= $bill->iva_total ?>">
+                </div>
 
+                <br>
 
-            <div class="form-group">
-                <label for="employee_reference_id">employee_reference_id</label>
-                <input type="text"
-                       class="form-control"
-                       id="employee_reference_id"
-                       name="employee_reference_id"
-                       value="<?= $bill->referencia_funcionario ?>">
-            </div>
+                <div class="form-group">
+                    <label for="state">Estado:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="state"
+                           name="state"
+                           value="<?= $bill->state ?>">
+                </div>
 
+                <br>
 
-            <br>
-            <button type="submit"
-                    class="btn btn-primary"
-                    name="return">voltar
-            </button>
+                <div class="form-group">
+                    <label for="client_reference_id">Referência Cliente:</label>
+                    <input type="text"
+                           class="form-control"
+                           id="client_reference_id"
+                           name="client_reference_id"
+                           value="<?= $bill->client_reference_id ?>">
+                </div>
 
-        </form>
+                <br>
+
+                <div class="form-group">
+                    <label for="employee_reference_id">Referência Funcionário</label>
+                    <input type="text"
+                           class="form-control"
+                           id="employee_reference_id"
+                           name="employee_reference_id"
+                           value="<?= $bill->employee_reference_id ?>">
+                </div>
+
+                <br>
+
+                <button type="submit"
+                        class="btn btn-primary"
+                        name="return">Voltar</button>
+
+            </form>
+        </div>
     </div>
-</div>
+</section>
 </body>
 </html>
