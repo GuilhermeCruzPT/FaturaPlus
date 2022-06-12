9d6fbc4bfe46f9c2f670dd8af2bcf8e6443da060
Claudia Correia <corrreiaclaudia2015@gmail.com>
2022-06-12 12:39:54 +0100

    pagina about us

--- website/views/site/about_us.php
+++ website/views/site/about_us.php
@@ -8,13 +8,13 @@
         <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
              viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
             <defs>
-                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"/>
             </defs>
             <g class="parallax">
-                <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
-                <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
-                <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
-                <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+                <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7"/>
+                <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)"/>
+                <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)"/>
+                <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff"/>
             </g>
         </svg>
     </div>
@@ -22,22 +22,21 @@
 </div>
 
 
-
-
-
 <div class="container p-5">
     <div class="row">
         <div class="col-md-8">
-            <h1 class="my-5"><?= APP_NAME ?> é a melhor empresa de faturação do mundo!</h1>
-            <p>
+            <h1 class="my-5">Contactos</h1>
+            <p>Telefone (+351) 244830010</p>
+            <p>E-mail ipleiria@ipleiria.pt</p>
 
-            </p>
+            <h1 class="my-5">Localizações</h1>
+            <p>Rua General Norton de Matos, Apartado 4133,
+                2411-901 Leiria – Portugal</p>
+            <p>GPS 39°44'15.1"N 8°48'40.8"W</p>
+
+
         </div>
-        <div class="col-md-4 text-center">
-            <div class="img-fluid logo">
-                <img class="rounded-circle img-fit" src="<?= DIRIMG ?>FaturaPlus.png" alt="<?= APP_NAME ?>" title="<?= APP_NAME ?>" >
-            </div>
-        </div>
+
     </div>
 </div>
 
